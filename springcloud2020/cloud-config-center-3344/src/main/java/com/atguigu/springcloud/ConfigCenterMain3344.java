ae4dfcd86228d5770f01f5eb267201e367c88fee
13974998269 <1341193285@qq.com>
2020-05-27 23:44:16 +0800

    代码的提交

--- springcloud2020/cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
+++ springcloud2020/cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * @Author ZL
